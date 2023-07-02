9a5e4936042b91184c81b2d54ab21f2c88d3efb0
Joshua Parr <131053642+Flighter13@users.noreply.github.com>
2023-07-02 17:00:19 -0400

    ad __init__

---
+++ __init__.py
@@ -0,0 +1,21 @@
+"""
+FJOSH: Fighter Jet Operating Systems HASH
+
+OS: Aviation, (Position), Communication, Radar, Munitions
+
+Aviation: control surfaces, instrumentation, status, performance
+(Position): GPS, orientation, 
+Communication: audio/visual representation,
+Radar: position, GPS, friendly/hostile detector,
+Munitions: aiming, position, performance, type, link to Aviation
+"""
+
+from Fighter import *
+from test import *
+
+
+
+
+
+
+
